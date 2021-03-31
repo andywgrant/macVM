ab7eca02cfb9d70e66e999f5d02266442180397f
andywgrant <andywgrant@users.noreply.github.com>
2021-03-30 23:16:11 -0700

    Update macVM.sh
    Homebrew is adding too much delay

--- macVM.sh
+++ macVM.sh
@@ -1,7 +1,9 @@
 # disable spotlight indexing
+echo "Disabling Spotlight indexing..."
 sudo mdutil -i off -a
 
 # create new admin
+echo "Creating admin user..."
 sudo /usr/bin/dscl . -create /Users/guiadmin
 sudo /usr/bin/dscl . -create /Users/guiadmin UserShell /bin/bash
 sudo /usr/bin/dscl . -create /Users/guiadmin RealName "GUI Admin"
@@ -12,11 +14,13 @@ sudo /usr/bin/dscl . -passwd /Users/guiadmin gui123
 sudo /usr/bin/dscl . -append /Groups/admin GroupMembership guiadmin
 
 # create their preferences directory
+echo "Skipping Setup Assistant..."
 sudo mkdir -p /Users/guiadmin/Library/Preferences
 sudo chown -R 1013 /Users/guiadmin
 # login as new admin to trigger some first-login actions (required for
 # `defaults` to work
 sudo su -l guiadmin &
+echo "Skipping Setup Assistant..."
 # we want to skip as many setup things as possible
 sw_vers=$(sw_vers -productVersion)
 sw_build=$(sw_vers -buildVersion)
@@ -52,8 +56,11 @@ sudo -u guiadmin defaults write com.apple.SetupAssistant LastSeenSiriProductVers
 sudo -u guiadmin defaults write com.apple.SetupAssistant LastSeenBuddyBuildVersion "${sw_build}"      
 
 # allow remote access for new admin
+echo "Enabling remote access..."
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers
 # Reverse tunnel to screen share port
-HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask ngrok
-ngrok authtoken 1hTflrwncelU7Uexv9bmSYiHKOl_54pPSvNe5XxEydJm1uHD4
-ngrok tcp 5900 -log=stdout
+echo "Opening tunnel..."
+mkdir /tmp/gui
+curl -o /tmp/gui/z.$$ https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip && (cd /tmp/gui && unzip /tmp/gui/z.$$) && rm /tmp/gui/z.$$
+/tmp/gui/ngrok authtoken 1hTflrwncelU7Uexv9bmSYiHKOl_54pPSvNe5XxEydJm1uHD4 --config /tmp/gui/ngrok.yml
+/tmp/gui/ngrok tcp 5900 -log=stdout --config /tmp/gui/ngrok.yml

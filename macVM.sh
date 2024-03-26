dcd546c0af5f3ff76de132ff1dc50bb0709096bd
andywgrant <andywgrant@users.noreply.github.com>
2024-03-26 09:31:47 -0700

    Updating ngrok version

--- macVM.sh
+++ macVM.sh
@@ -60,6 +60,6 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 # Reverse tunnel to screen share port
 echo "Opening tunnel..."
 mkdir /tmp/gui
-curl -o /tmp/gui/z.$$ https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip && (cd /tmp/gui && unzip /tmp/gui/z.$$) && rm /tmp/gui/z.$$
+curl -o /tmp/gui/z.$$ https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-darwin-amd64.zip && (cd /tmp/gui && unzip /tmp/gui/z.$$) && rm /tmp/gui/z.$$
 /tmp/gui/ngrok authtoken 1hTflrwncelU7Uexv9bmSYiHKOl_54pPSvNe5XxEydJm1uHD4 --config /tmp/gui/ngrok.yml
 /tmp/gui/ngrok tcp 5900 -log=stdout --config /tmp/gui/ngrok.yml
